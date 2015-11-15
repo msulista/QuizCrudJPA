5e5a2bf7820fa205b987ea8d1cd2e6921cc596c7
msulista <marcus.rodrigues81@gmail.com>
2015-11-15 16:57:00 -0200

    Marcus - Refatorando Turma retirando numero de alunos para calcular com a lista

--- src/java/model/Turma.java
+++ src/java/model/Turma.java
@@ -9,15 +9,11 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 /**
  *
@@ -54,9 +50,16 @@ public class Turma implements Serializable{
         this.curso = curso;
         this.semestre = semestre;
         this.alunos = new ArrayList<>();
-        //quantidadeAlunos();
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
     public String getNome() {
         return nome;
     }
