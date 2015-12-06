2c932bc1b4a824965d3e899335d8c404dd6456d9
msulista <marcus.rodrigues81@gmail.com>
2015-12-06 17:37:31 -0200

    Marcus - Adicionando classe ClienteUsuario para conectar com webService

---
+++ src/java/ws/ClienteUsuario.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws;
+
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import model.Usuario;
+import sun.net.www.content.text.Generic;
+
+/**
+ * Jersey REST client generated for REST resource:UsuariosResource
+ * [usuarios]<br>
+ * USAGE:
+ * <pre>
+ *        ClienteUsuario client = new ClienteUsuario();
+ *        Object response = client.XXX(...);
+ *        // do whatever with response
+ *        client.close();
+ * </pre>
+ *
+ * @author marcus.rodrigues
+ */
+public class ClienteUsuario {
+    private WebTarget webTarget;
+    private Client client;
+    private static final String BASE_URI = "http://localhost:8080/ProdutoRest/webresources";
+
+    public ClienteUsuario() {
+        client = javax.ws.rs.client.ClientBuilder.newClient();
+        webTarget = client.target(BASE_URI).path("usuarios");
+    }
+
+    public <T> T getUsuarios(GenericType<T> responseType) throws ClientErrorException {
+        WebTarget resource = webTarget;
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
+    }
+
+    public String addUsuario(Usuario u) throws ClientErrorException {
+        return webTarget.request(javax.ws.rs.core.MediaType.TEXT_PLAIN).post(javax.ws.rs.client.Entity.entity(u, javax.ws.rs.core.MediaType.APPLICATION_JSON), String.class);
+    }
+
+    public void close() {
+        client.close();
+    }
+    
+}
